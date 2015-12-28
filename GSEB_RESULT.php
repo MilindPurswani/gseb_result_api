0c78508047ab5ff7a168239cecef31b1200427bf
Yash Sodha <y_sodha@yahoo.com>
2015-12-28 21:36:31 +0530

    Minor Curl Change
    Closed Curl Connection before processing.

--- GSEB_RESULT.php
+++ GSEB_RESULT.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 
 if (isset($_GET['roll'])) {
 	$rol = stripcslashes($_GET['roll']);
@@ -12,23 +12,30 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 curl_setopt ($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+curl_close($ch);
+
 $value = curl_exec($ch);
 $regexp_marks='/\<td align\=\'center\'\>(.*?)\<\/td\>/';
 $regexp_name = '/\<\/b\>(.*?)\<\/span\>/';
-$regexp_sub = '/\<tr\>\<td align\=\'left\'\>(.*?)\<\/td\>/';
+$regexp_sub = '/\<tr\>\<td align\=\'left\'\>(.*?)\<\/td\>/'; 
 preg_match_all($regexp_name,$value,$matches_name);
 preg_match_all($regexp_marks, $value,$matches);
 preg_match_all($regexp_sub, $value,$matches_sub);
 
 $arr = array();
 
-foreach($matches[1] as $key => $mat){
-		if($key%3==0){
+foreach($matches[1] as $key => $mat)
+{
+		if($key%3==0)
+		{
 			//echo $mat."<br
 		array_push($arr, $mat);
 		}
 }
+
 preg_match_all('!\d+!',implode($matches_sub[1]),$arr4);
+
 $counter = count($matches_sub[1]);
 $arr2 = array_slice($arr,0,$counter);
 $arr3 = array_combine($arr4[0],$arr2);
@@ -40,7 +47,4 @@ $arr3['total'] = $matches[1][count($matches[1])-2];
 
 echo json_encode($arr3);
 	
-
-
-curl_close($ch);
 ?>
